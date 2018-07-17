c950aef3de24b9a1e5f4f9f22d9bbc2b14f3d90c
Ryan Haley <ryanhaley@frazer.biz>
2018-07-16 21:22:23 -0400

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+import logo from '../images/logo.png'
+
+const HeaderWrapper = styled.div`
+  background: #fff;
+  margin-bottom: 1.45rem;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Header = () => (
+  <HeaderWrapper>
+    <HeaderContainer>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          <img src={logo} alt="Taints and Tolerations Logo" />
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  </HeaderWrapper>
+)
+
+export default Header
---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { SSL_OP_PKCS1_CHECK_1 } from 'constants'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Quinoa man bun single-origin coffee, seitan drinking vinegar copper
+          mug master cleanse lumbersexual taiyaki cornhole. Skateboard stumptown
+          waistcoat salvia chambray austin tacos. Four dollar toast snackwave
+          kombucha gluten-free you probably haven't heard of them. Kickstarter
+          keffiyeh subway tile, 3 wolf moon twee cliche stumptown cardigan
+          chicharrones four loko venmo brunch air plant.
+        </p>
+        <p>
+          90's aesthetic beard, prism 8-bit thundercats heirloom flannel salvia
+          schlitz. Vaporware yr flexitarian unicorn try-hard cliche. Stumptown
+          live-edge locavore marfa trust fund fingerstache. Lo-fi yuccie cronut,
+          austin small batch master cleanse affogato poke cliche drinking
+          vinegar viral quinoa post-ironic umami. Williamsburg shaman roof party
+          fam mumblecore helvetica master cleanse blue bottle yuccie twee.
+        </p>
+      </div>
+    )
+  }
+}
